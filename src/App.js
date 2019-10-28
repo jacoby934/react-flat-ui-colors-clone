02376544fe8a1b76bd042f40bf6578af6c547706
David Jacoby <jacoby934@gmail.com>
2019-10-28 14:58:56 -0400

    Created palette component and added routing

--- src/App.js
+++ src/App.js
@@ -1,13 +1,33 @@
-import React from "react";
+import React, { Component } from "react";
+import { Route, Switch } from "react-router-dom";
 import Palette from "./Palette";
 import seedColors from "./seedColors";
+import { generatePalette } from "./ColorHelpers";
 
-function App() {
-  return (
-    <div className="App">
-      <Palette {...seedColors[4]} />
-    </div>
-  );
+export default class App extends Component {
+  findPalette(id) {
+    return seedColors.find(function(palette) {
+      return palette.id === id;
+    });
+  }
+  render() {
+    return (
+      <div>
+        <Switch>
+          <Route exact path="/" render={() => <h1>Palette</h1>} />
+          <Route
+            exact
+            path="/palette/:id"
+            render={routeProps => (
+              <Palette
+                palette={generatePalette(
+                  this.findPalette(routeProps.match.params.id)
+                )}
+              />
+            )}
+          />
+        </Switch>
+      </div>
+    );
+  }
 }
-
-export default App;
